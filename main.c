44148e4786136a74403ac9776f20d573395d6c3c
Sergey Bugaev <bugaevc@gmail.com>
2017-08-23 15:59:13 +0300

    Obtain a few global objects from the registry

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+
 #include <wayland-client.h>
+
+struct wl_compositor *compositor;
+struct wl_shm *shm;
+struct wl_shell *shell;
 
 void registry_global_handler
 (
@@ -9,7 +15,16 @@ void registry_global_handler
     const char *interface,
     uint32_t version
 ) {
-    printf("interface: '%s', version: %u, name: %u\n", interface, version, name);
+    if (strcmp(interface, "wl_compositor") == 0) {
+        compositor = wl_registry_bind(registry, name,
+            &wl_compositor_interface, 3);
+    } else if (strcmp(interface, "wl_shm") == 0) {
+        shm = wl_registry_bind(registry, name,
+            &wl_shm_interface, 1);
+    } else if (strcmp(interface, "wl_shell") == 0) {
+        shell = wl_registry_bind(registry, name,
+            &wl_shell_interface, 1);
+    }
 }
 
 void registry_global_remove_handler
@@ -17,19 +32,29 @@ void registry_global_remove_handler
     void *data,
     struct wl_registry *registry,
     uint32_t name
-) {
-    printf("removed: %u\n", name);
-}
+) {}
+
+const struct wl_registry_listener registry_listener = {
+    .global = registry_global_handler,
+    .global_remove = registry_global_remove_handler
+};
 
 int main(void)
 {
     struct wl_display *display = wl_display_connect(NULL);
     struct wl_registry *registry = wl_display_get_registry(display);
-    struct wl_registry_listener registry_listener = {
-        .global = registry_global_handler,
-        .global_remove = registry_global_remove_handler
-    };
     wl_registry_add_listener(registry, &registry_listener, NULL);
+
+    // wait for the "initial" set of globals to appear
+    wl_display_roundtrip(display);
+
+    // all our objects should be ready!
+    if (compositor && shm && shell) {
+        printf("Got them all!\n");
+    } else {
+        printf("Some required globals unavailable\n");
+        return 1;
+    }
 
     while (1) {
         wl_display_dispatch(display);
