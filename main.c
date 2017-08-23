3676aa83bba279abc608e53e99e3ea0b005ada50
Sergey Bugaev <bugaevc@gmail.com>
2017-08-23 16:16:25 +0300

    Draw a cross

--- main.c
+++ main.c
@@ -147,22 +147,16 @@ int main(void)
                 unsigned char alpha;
             } *px = (struct pixel *) (data + y * stride + x * 4);
 
-            // draw a stripes pattern
-            if ((x + y) % 30 < 10) {
+            // draw a cross
+            if ((80 < x && x < 120) || (80 < y && y < 120)) {
+                // gradient from blue at the top to white at the bottom
+                px->alpha = 255;
+                px->red = (double) y / height * 255;
+                px->green = px->red;
+                px->blue = 255;
+            } else {
                 // transparent
                 px->alpha = 0;
-            } else if ((x + y) % 30 < 20) {
-                // yellow
-                px->alpha = 255;
-                px->red = 255;
-                px->green = 255;
-                px->blue = 0;
-            } else {
-                // semitransparent red
-                px->alpha = 128;
-                px->red = 255;
-                px->green = 0;
-                px->blue = 0;
             }
         }
     }
