918bc82c8c2b3046d843fc33f6c25f3816f34f1b
Sergey Bugaev <bugaevc@gmail.com>
2017-08-23 15:54:44 +0300

    List global objects

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
+void registry_global_handler
+(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t name,
+    const char *interface,
+    uint32_t version
+) {
+    printf("interface: '%s', version: %u, name: %u\n", interface, version, name);
+}
+
+void registry_global_remove_handler
+(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t name
+) {
+    printf("removed: %u\n", name);
+}
+
 int main(void)
 {
     struct wl_display *display = wl_display_connect(NULL);
-    if (display) {
-        printf("Connected!\n");
-    } else {
-        printf("Error connecting ;(\n");
-        return 1;
-    }
+    struct wl_registry *registry = wl_display_get_registry(display);
+    struct wl_registry_listener registry_listener = {
+        .global = registry_global_handler,
+        .global_remove = registry_global_remove_handler
+    };
+    wl_registry_add_listener(registry, &registry_listener, NULL);
 
-    wl_display_disconnect(display);
-    return 0;
+    while (1) {
+        wl_display_dispatch(display);
+    }
 }
